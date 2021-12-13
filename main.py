7761334760943bb69a03bd3bbe36ce83cc4f9ee8
MatthewMachuca <66333580+MatthewMachuca@users.noreply.github.com>
2021-12-13 15:43:22 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+
+
+import json
+import pandas as pd
+from flatten_json import flatten
+
+def json_tocsv(filename):
+
+    file = filename
+
+    partition = file.partition('.')
+
+    csv_filename = partition[0]
+    f = open(file)
+    data= json.load(f)
+
+    flat = flatten(data)
+
+
+
+    flat_values = flat.values()
+    flat_keys = flat.keys()
+    types = {}
+
+    t_hold = []
+
+    for item in flat:
+        t_hold.append(type(flat[item]))
+
+
+
+
+    df = pd.DataFrame(
+        {"Attribute": flat_keys,
+        'Sample Value': flat_values,
+        'Data Type': t_hold }
+    )
+
+    #df.to_csv('test6.csv')
+
+    df.to_csv(f'{csv_filename}.csv')
+
+    print(df)
+    main()
+
+def main():
+    print('\n')
+    print('\n')
+
+    value = input('Press 1 to convert a file from json to csv, Press 2 to quit: ')
+    print('\n')
+    if value == '1':
+        f = input('please enter the file name,please include the extension i;e [.json] :')
+        print('\n')
+        json_tocsv(f)
+    elif value == '2':
+        exit()
+    else:
+        main()
+
+main()
